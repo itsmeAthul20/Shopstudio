ef6e2c0474038f12fe3ce99f2638e99a5c4b6102
itsmeAthul20 <athulharidas20@gmail.com>
2024-10-24 19:15:43 +0530

    Your commit message here

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer'
 import Ads from '../components/Ads'
 import Ad from '../components/Ad'
-import '../styles/Home.css';
+import '../Styles/Home.css';
 
 
 const Home = () => {
